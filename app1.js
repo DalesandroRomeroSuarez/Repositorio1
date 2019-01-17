34e21c45e21e3c956b36af4bcb630dd9bd33f1cd
DalesandroRomeroSuarez <tuandyou@hotmail.com>
2019-01-16 23:46:26 -0500

    subiendo ramas

--- app1.js
+++ app1.js
@@ -1,1 +1,2 @@
-console.log('modificando desde Dalesandro Romero Suarez app.js merge.');
+console.log('modificando desde Dalesandro Romero Suarez app1.js merge.');
+console.log('Ramas desde Dalesandro Romero Suarez app1.js');
